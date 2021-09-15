71f42de2ae03f0ba7473feec46f701c1818fb1de
a.makhova <a.makhova1@yandex.ru>
2021-09-15 19:54:55 +0300

    Hello.java

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 class Hello {
     public static void main(String[] args) {
 	System.out.println("A");
-        System.out.println("Hello, World!1");
+        System.out.println("Hello, World!2");
 <<<<<<< HEAD
 	System.out.println("B");
 =======
